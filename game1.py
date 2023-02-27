6540f7fc993e52b5041e09d2b4c7f4f6f0d78f53
shahid ali <alishahid314>
2023-02-27 11:13:35 +0530

    DSDE

--- game1.py
+++ game1.py
@@ -1,25 +1,31 @@
 import pgzrun
 
-music.play('song.mp3')
+# add bgm music
+music.play('bg')
 
-
-b = Rect((50,50),(100,50))
-vx = 3
-vy = 5
+b = Rect((50,50), (100,50))
+vx, vy = 3, 6
 
 def draw():
     screen.fill('black')
-    screen.draw.filled_rect(b, 'blue')
+    screen.draw.filled_rect(b,'green')
 
-def update ():
-    global vx, vy
+def update():
+    global vx , vy
     b.x += vx
-    b.y += vy
-    if b.right>800 or b.left <0:
+    b.y += vx
+    if b.right >800 or b.left < 0:
         vx = -vx
-    if b.bottom >600 or b.top <0:
-        vy = -vy
+        sounds.s1.play()
 
+    if b.bottom > 600 or b.top < 0:
+        vy= -vy
+        sounds.s1.play() 
 
-#outside of all function
 pgzrun.go()
+
+    
+
+
+
+
