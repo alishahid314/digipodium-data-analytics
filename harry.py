59ee209016d504d829769fd17db42a0cceec0936
shahid ali <alishahid314>
2023-03-07 10:55:39 +0530

    dictionary

--- harry.py
+++ harry.py
@@ -1,2 +1,4 @@
-from playsound import
-playsound('F:\\music')
+from playsound import *
+playsound('gallery\sounds\smoosh.wav.wav')
+playsound('gallery\sounds\wing.wav.mp3')
+playsound('gallery\sounds\smoosh.wav.wav')
